178a92088e8ebe3a4cb5cb5dcaa392b29911bd21
Wade Xing <xing.xing@condenast.com.cn>
2017-09-20 12:12:42 +0800

    chore: publish deps

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Ronglian.Mixfile do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [app: :ronglian,
-     version: "0.2.0",
+     version: @version,
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -33,6 +37,22 @@ defmodule Ronglian.Mixfile do
      {:inflex, "~> 1.8.1"},
      {:poison, "~> 2.2"},
      {:sweet_xml, "~> 0.6.5"},
+     {:ex_doc, ">= 0.0.0", only: :dev},
      {:timex, "~> 3.0"},]
+  end
+
+  defp package do
+    [# These are the default files included in the package
+      name: :ronglian,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Wade Xing"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ZhuyiHome/ronglianyun"}]
+  end
+
+  defp description do
+    """
+    容联云 Elixir Client
+    """
   end
 end
